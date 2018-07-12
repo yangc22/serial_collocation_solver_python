01e9c00fe03aada3a84ab20674427508e752e903
yangc22 <yangchaoyihit@gmail.com>
2018-07-12 15:11:30 -0700

    stuck on sym

--- generate_bvp.py
+++ generate_bvp.py
@@ -7,9 +7,35 @@ TerminalConstraints     = "[x1, x2]"
 CostFunctional          = "u1*u1*0.5"
 DifferentialEquations   = "[x2, u1]"
 
+# state variables of the OCP
 sv = symbols(StateVariables)
+# control variables of the OCP
 cv = symbols(ControlVariables)
+# initial constraints of the OCP
 ic = symbols(InitialConstraints)
+# terminal constraints of the OCP
 tc = symbols(TerminalConstraints)
+# cost function of the OCP
 cf = symbols(CostFunctional)
+# differential equation of the OCP
 de = symbols(DifferentialEquations)
+
+# number of the state variables
+size_sv = len(sv)
+# number of the control variables
+size_cv = len(sv)
+# number of the initial constraints
+size_ic = len(ic)
+# number of the terminal constarints
+size_tc = len(tc)
+
+# Lagrange multipliers with state variables
+_lambda = symarray('_lambda', size_sv)
+
+# Hamiltonian function with Lagrange multipliers
+H = cf + _lambda * de
+
+de1 = sv[1]
+h = diff(de1, sv[1])
+print(de1, sv[1])
+print(h)
